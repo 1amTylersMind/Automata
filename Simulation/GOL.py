c3b3c6c55eec517f2b22ea30973170c08e8148f0
root <root@localhost.localdomain>
2018-11-17 04:13:01 +0000

    Fixed GameOfLife, and made it more customizable

--- Simulation/GOL.py
+++ Simulation/GOL.py
@@ -1,5 +1,5 @@
 import numpy as np, matplotlib.pyplot as plt, matplotlib.animation as animation
-import scipy.ndimage as ndi
+import scipy.ndimage as ndi, time, resource
 
 
 def render(matrices, speedOfLife):
@@ -15,20 +15,23 @@ def render(matrices, speedOfLife):
 def initialize_life():
     width = int(input('Enter width: '))
     height = int(input('Enter height: '))
-    initial_state = np.random.randint(0, 2, width * height).reshape((width, height))
+    initial_state = np.random.randn(width * height).reshape((width, height)) > 0.5
     plt.imshow(initial_state, 'gray_r')
     plt.title('Initial State [GOL]')
     plt.show()
-    return initial_state
+
+    initial = np.array(initial_state,dtype=int)
+    return initial
 
 
-def run(nGenerations, seed):
+def run(nGenerations, speed, seed):
     neighbors = [[1, 1, 1],
                  [1, 0, 1],
                  [1, 1, 1]]
 
     generations = []
     gen = 0
+    start = time.time()
     while gen <= nGenerations:
         cells = ndi.convolve(seed, neighbors)
         nextState = seed.flatten()
@@ -40,21 +43,29 @@ def run(nGenerations, seed):
                     nextState[II] = 1
                 else:
                     nextState[II] = 0
-            if nextState[II] == 0 and cell == 2:
+            elif cell == 3:
                 nextState[II] = 1
-            else:
-                nextState[II] = 0
             II += 1
         generations.append(nextState.reshape((seed.shape[0], seed.shape[1])))
         gen += 1
         seed = nextState.reshape((seed.shape[0], seed.shape[1]))
     # Animate the Game of Life Simulation!
-    render(generations, 100)
+    render(generations, speed)
+    print "Finished Simulating" + str(nGenerations)+\
+          " [" + str(time.time() - start) + "s Elapsed]"
+
+
+def check_mem_usage():
+    mem = resource.getrusage(resource.RUSAGE_SELF).ru_maxrss
+    return mem
 
 
 def main():
+    print str(float(100 * check_mem_usage())/100000) +" Kb of initial RAM Overhead"
     initial_state = initialize_life()
-    run(int(input('Enter Number of Generations to Simulate: ')), initial_state)
+    run(int(input('Enter Number of Generations to Simulate: ')),
+        int(input('Enter Speed of Life [0-100]:\n') * 100 / 100), initial_state)
+    print str(float(100*check_mem_usage())/100000) + " Kb of RAM Used"
 
 
 if __name__ == '__main__':
